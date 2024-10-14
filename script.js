fa1a3409133fafda9dd5318e5c04db15fc2d2bf7
Igor Kohanskiy <shedaesq@mail.ru>
2024-10-14 22:56:56 +0700

    Сверстаны шапка, основная часть сайта и подвал. Применены необходимые стили. Реализован основной функционал сайта

---
+++ script.js
@@ -0,0 +1,7 @@
+alert ("Добро пожаловать на сайт! Тут есть 10 кнопочек, но работает только одна... Найди её!")
+
+const button = document.getElementById("button")
+
+button.addEventListener("click", function () {
+alert("Замечательно!")
+})
